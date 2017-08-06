12a3aa3ec3d6db96e7e5ec0e5790e2fe06f0701e
Staffan Thomen <duck@shangtai.net>
2017-08-06 13:40:30 +0300

    Tweaked watchface layout: - Changed the white background to be an arc - Added step counter arc (percentage of average daily steps) - Increased font size for the hours and the date - Added a white wedge inside the "slow time" pointer

--- src/slow-time.c
+++ src/slow-time.c
@@ -6,9 +6,9 @@
  * XXX Always use parens when the values are calculations or there may be
  * arithmetic ordering issues.
  */
-#define TRACK_WIDTH 20
+#define TRACK_WIDTH 10
 #define TRACK_OUTER center.x
-#define TRACK_INNER (center.x-TRACK_WIDTH)
+#define TRACK_INNER (center.x-(TRACK_WIDTH*2))
 
 #define POINTER_WIDTH 30
 #define POINTER_HEIGHT 18
@@ -16,14 +16,12 @@
 #define MINUTES_MAGIC_NUMBER 1.5
 #define DATE_MAGIC_NUMBER 3
 
-#define HOUR_OFFSET (TRACK_INNER-10)
+#define HOUR_OFFSET (TRACK_INNER-TRACK_WIDTH)
 
-#define BATTERY_BORDER 4
-
-#define FONT fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD)
-#define FONT_HOUR fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD)
+#define FONT fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD)
+#define FONT_HOUR fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD)
 #define FONT_MINUTES fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD)
-#define FONT_DATE fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD)
+#define FONT_DATE fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD)
 
 Window *window_main;
 GRect bounds;			// holds the window bounds for dynamic size (high-res pebbles?)
@@ -35,14 +33,12 @@ TextLayer *layer_hour;
 TextLayer *layer_minutes;
 TextLayer *layer_date;
 
-GPath *path_triangle=NULL;
-GPathInfo *path_triangle_info;
-
 char hour_string[3];		// two digits and null
 char minute_string[3];		// two digits and null
 char date_string[7];		// three characters, space two digits and null
 
 static int8_t charge=0;
+static int32_t stepspercent=0;
 static bool charging=false;
 
 struct tm now;
@@ -51,20 +47,20 @@ struct tm now;
  * Utility
  ***********************************************************************/
 
-static GPathInfo *generate_offset_pointer()
+static GPathInfo *generate_offset_pointer(int offset)
 {
 	GPathInfo *path=malloc(sizeof(GPathInfo));
 	path->num_points=3;
 	path->points=malloc(sizeof(GPoint)*3);
 
 	path->points[0].x=0;
-	path->points[0].y=TRACK_INNER;
+	path->points[0].y=TRACK_INNER+offset;
 
 	path->points[1].x=-(POINTER_WIDTH/2);
-	path->points[1].y=TRACK_INNER+POINTER_HEIGHT;
+	path->points[1].y=TRACK_INNER+POINTER_HEIGHT+offset;
 
 	path->points[2].x=POINTER_WIDTH/2;
-	path->points[2].y=TRACK_INNER+POINTER_HEIGHT;
+	path->points[2].y=TRACK_INNER+POINTER_HEIGHT+offset;
 
 	return path;
 }
@@ -98,6 +94,35 @@ static void bluetooth_handler(bool connected)
 	vibes_short_pulse();
 }
 
+#ifdef PBL_HEALTH
+static bool health_metric_available(HealthMetric metric, HealthServiceTimeScope scope, time_t start, time_t end) {
+	return (health_service_metric_accessible(metric, start, end) & HealthServiceAccessibilityMaskAvailable)
+		&& (health_service_metric_averaged_accessible(metric, start, end, scope) & HealthServiceAccessibilityMaskAvailable);
+}
+
+static void health_handler(HealthEventType event, void *context) {
+	int32_t today, average;
+
+	time_t start = time_start_of_today();
+	time_t end = time(NULL);
+
+	const HealthMetric metric = HealthMetricStepCount;
+	const HealthServiceTimeScope scope = HealthServiceTimeScopeDaily;
+
+	DEBUG_INFO("health_handler");
+
+	if (health_metric_available(metric, scope, start, end)) {
+
+		today = health_service_sum_today(metric);
+		average = health_service_sum_averaged(metric, start, end, scope);
+
+		stepspercent = (int32_t)(((float)today / (float)average) * 100.0);
+
+		DEBUG_INFO("health_handler: today: %d, average: %d, percent: %d", (int)today, (int)average, (int)stepspercent);
+	}
+}
+#endif
+
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
 	memcpy(&now, tick_time, sizeof(struct tm));
@@ -113,52 +138,87 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 
 static void update_background(Layer *this, GContext *ctx)
 {
-	int start_angle;
+	int battery_angle;
+	int steps_angle;
+
+	battery_angle=180 - (charge * 3.6);
+	steps_angle=180 - (stepspercent * 3.6);
 
 	// draw background
 	graphics_context_set_fill_color(ctx, GColorBlack);
 	graphics_fill_rect(ctx, bounds, 0, 0);
 
-	// draw track
 	graphics_context_set_fill_color(ctx, GColorWhite);
-	graphics_fill_circle(ctx, center, TRACK_OUTER);
 
-	graphics_context_set_fill_color(ctx, GColorBlack);
-	graphics_fill_circle(ctx, center, TRACK_INNER);
+	graphics_fill_radial(ctx,
+		GRect(	center.x-TRACK_OUTER,
+			center.y-TRACK_OUTER,
+			center.x+TRACK_OUTER,
+			center.y+TRACK_OUTER),
+		GOvalScaleModeFitCircle,
+		TRACK_WIDTH*2,
+		DEG_TO_TRIGANGLE(0),
+		DEG_TO_TRIGANGLE(360));
 
-#ifdef PBL_COLOR
-	start_angle=180 - (charge * 3.6);
 
 	// draw battery state
+#ifdef PBL_COLOR
 	graphics_context_set_fill_color(ctx, GColorOrange);
-	graphics_fill_radial(ctx,
-		GRect(	center.x-TRACK_OUTER+BATTERY_BORDER,
-			center.y-TRACK_OUTER+BATTERY_BORDER,
-			center.x+TRACK_OUTER-(BATTERY_BORDER*2),
-			center.x+TRACK_OUTER-(BATTERY_BORDER*2)),
-		GOvalScaleModeFitCircle,
-		TRACK_WIDTH-(BATTERY_BORDER*2),
-		DEG_TO_TRIGANGLE(start_angle),
-		DEG_TO_TRIGANGLE(180));
+#else
+	graphics_context_set_fill_color(ctx, GColorBlack);
 #endif
+
+	graphics_fill_radial(ctx,
+		GRect(	center.x-TRACK_OUTER,
+			center.y-TRACK_OUTER,
+			center.x+TRACK_OUTER,
+			center.y+TRACK_OUTER),
+		GOvalScaleModeFitCircle,
+		TRACK_WIDTH,
+		DEG_TO_TRIGANGLE(battery_angle),
+		DEG_TO_TRIGANGLE(180));
+
+	// draw steps graph
+#ifdef PBL_COLOR
+	graphics_context_set_fill_color(ctx, GColorMalachite);
+#else
+	graphics_context_set_fill_color(ctx, GColorBlack);
+#endif
+
+	graphics_fill_radial(ctx,
+		GRect(	center.x-TRACK_OUTER+TRACK_WIDTH,
+			center.y-TRACK_OUTER+TRACK_WIDTH,
+			center.x+TRACK_OUTER-(TRACK_WIDTH*2),
+			center.y+TRACK_OUTER-(TRACK_WIDTH*2)),
+		GOvalScaleModeFitCircle,
+		TRACK_WIDTH,
+		DEG_TO_TRIGANGLE(steps_angle),
+		DEG_TO_TRIGANGLE(180));
 }
 
 static void update_pointer(Layer *this, GContext *ctx)
 {
 	int total_minutes;
+	GPath *path_triangle=NULL;
+	GPath *path_triangle_inset=NULL;
 
 	total_minutes=now.tm_hour * 60 + now.tm_min;
 
-	if (path_triangle != NULL)
-		gpath_destroy(path_triangle);
-
-	path_triangle=gpath_create(path_triangle_info);
+	path_triangle=gpath_create(generate_offset_pointer(0));
 
 	gpath_rotate_to(path_triangle, TRIG_MAX_ANGLE / 1440 * total_minutes);
-
 	gpath_move_to(path_triangle, center);
+
 	graphics_context_set_fill_color(ctx, GColorBlack);
 	gpath_draw_filled(ctx, path_triangle);
+
+	path_triangle_inset=gpath_create(generate_offset_pointer(5));
+
+	gpath_rotate_to(path_triangle_inset, TRIG_MAX_ANGLE / 1440 * total_minutes);
+	gpath_move_to(path_triangle_inset, center);
+
+	graphics_context_set_fill_color(ctx, GColorWhite);
+	gpath_draw_filled(ctx, path_triangle_inset);
 }
 
 /************************************************************************
@@ -177,8 +237,6 @@ static void window_main_load(Window *window)
 	// load window size
 	bounds=layer_get_bounds(root);
 	center=GPoint(bounds.size.w/2, bounds.size.h/2);
-	// XXX this must be run after center is initialized
-	path_triangle_info=generate_offset_pointer();
 
 	t=time(NULL);
 	memcpy(&now, localtime(&t), sizeof(struct tm));
@@ -271,6 +329,10 @@ static void init()
 
 	bluetooth_connection_service_subscribe(bluetooth_handler);
 
+#ifdef PBL_HEALTH
+	health_service_events_subscribe(health_handler, NULL);
+#endif
+
 	window_main = window_create();
 	window_set_window_handlers(window_main, window_handlers);
 	window_stack_push(window_main, true);
@@ -282,6 +344,10 @@ static void deinit()
 	tick_timer_service_unsubscribe();
 	battery_state_service_unsubscribe();
 	bluetooth_connection_service_unsubscribe();
+
+#ifdef PBL_HEALTH
+	health_service_events_unsubscribe();
+#endif
 }
 
 int main(void)
